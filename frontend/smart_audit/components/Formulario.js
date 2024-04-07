3998087dd268d3d72eae93a381f515132c4121b6
diegoalerg <diegorodriguezgalindez@gmail.com>
2024-04-07 16:29:29 +0200

    feat - installed VSCode dependencies, with Node and npm

--- frontend/smart_audit/components/Formulario.js
+++ frontend/smart_audit/components/Formulario.js
@@ -1,134 +1,110 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import styles from "../styles/Home.module.css";
 
-import { MuiFileInput } from 'mui-file-input'
+import { MuiFileInput } from "mui-file-input";
 
-import { TextField } from '@mui/material'
+import { TextField } from "@mui/material";
 
-import axios from 'axios';
+import axios from "axios";
 
 const Formulario = ({ resultFunction, user_address }) => {
+  const [smartContractAddress, setSmartContractAddress] = useState("");
+  const [auditFile, setAuditFile] = useState(null);
 
-    const [smartContractAddress, setSmartContractAddress] = useState("");
-    const [auditFile, setAuditFile] = useState(null);
+  const [error, guardarError] = useState(false);
 
-    const [error, guardarError] = useState(false);
+  const submitForm = (e) => {
+    // Prevent redirection
+    e.preventDefault();
 
+    // Check that we have an address
+    if (smartContractAddress.trim() === "") {
+      guardarError(true);
+      return;
+    }
+    guardarError(false);
+    //Todo bien, pasar al componente principal
 
-    const submitForm = e => {
+    const formData = new FormData();
+    formData.append("user_address", user_address);
+    formData.append("contract_address", smartContractAddress);
+    formData.append("audit", auditFile);
 
-        // Prevent redirection
-        e.preventDefault();
+    console.log(formData);
 
-        // Check that we have an address
-        if (smartContractAddress.trim() === '') {
-            guardarError(true);
-            return;
-        }
-        guardarError(false);
-        //Todo bien, pasar al componente principal
+    // console.log("Result:" + result );
 
-        const formData = new FormData();
-        formData.append("user_address", user_address);
-        formData.append("contract_address", smartContractAddress);
-        formData.append("audit", auditFile);
+    axios
+      .post("http://localhost:8000/audit/upload/address", formData, {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "multipart/form-data",
+        },
+      })
 
-        console.log(formData);
+      .then((res) => {
+        alert("File Upload success");
+        console.log(res);
+        // result = true
 
+        resultFunction(true);
+
+        // setState
         // console.log("Result:" + result );
-        
+      })
+      .catch((err) => {
+        console.log(err);
+        // result = false
+        // console.log("Result:" + result );
 
-        axios
-            .post(
-                "http://localhost:8000/audit/upload/address",
-                formData,
-                {
-                    headers: {
-                        "Access-Control-Allow-Origin": "*",
-                        "Content-Type": "multipart/form-data"
-                    }
-                })
+        resultFunction(false);
+      });
+  };
 
-            .then((res) => {
-                alert("File Upload success");
-                console.log(res);
-                // result = true
+  return (
+    <div className="bg-info">
+      {error ? <p className={styles.grid}>There is no hash or file to audit</p> : null}
+      <div className="container">
+        <div className="row">
+          <form onSubmit={submitForm} className="col card text-white bg-transparent mb-5 pt-5 pb-2">
+            <fieldset>
+              <legend className="text-center">Smart Contract Auditor</legend>
 
-                resultFunction(true);
+              <div className="row">
+                <div className="col-md-6">
+                  <div className="form-group">
+                    <h2>Smart Contract Hash</h2>
 
-                // setState
-                // console.log("Result:" + result );
-            })
-            .catch((err) => {
-                console.log(err);
-                // result = false
-                // console.log("Result:" + result );
-
-                resultFunction(false);
-            });
-    };
-
-
-
-    return (
-        <div className="bg-info">
-            {error ? <p className={styles.grid}>There is no hash or file to audit</p> : null}
-            <div className="container">
-                <div className="row">
-
-                    <form
-                        onSubmit={submitForm}
-                        className="col card text-white bg-transparent mb-5 pt-5 pb-2"
-                    >
-                        <fieldset>
-                            <legend className="text-center">Smart Contract Auditor
-                            </legend>
-
-                            <div className="row">
-                                <div className="col-md-6">
-                                    <div className="form-group">
-                                        <h2>Smart Contract Hash</h2>
-
-                                        <TextField
-                                            id="outlined-basic"
-                                            // label="Smart Contract Hash"
-                                            variant="outlined"
-                                            placeholder="0X24567..."
-                                            fullWidth={true}
-                                            onChange={(e) => setSmartContractAddress(e.target.value)}
-                                            value={smartContractAddress}
-                                        />
-                                    </div>
-                                </div>
-                                <div className="col-md-6">
-                                    <div className="form-group">
-                                        <h2>Upload the audit</h2>
-                                        <MuiFileInput
-                                            value={auditFile}
-                                            onChange={(newFile) => setAuditFile(newFile)}
-                                        />
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div className={styles.grid}>
-                                <button
-                                    type="submit"
-                                    className={styles.grid}> Contribute
-                                </button>
-                            </div>
-
-                        </fieldset>
-
-
-                    </form>
-
-
+                    <TextField
+                      id="outlined-basic"
+                      // label="Smart Contract Hash"
+                      variant="outlined"
+                      placeholder="0X24567..."
+                      fullWidth={true}
+                      onChange={(e) => setSmartContractAddress(e.target.value)}
+                      value={smartContractAddress}
+                    />
+                  </div>
                 </div>
-            </div>
-
+                <div className="col-md-6">
+                  <div className="form-group">
+                    <h2>Upload the audit</h2>
+                    <MuiFileInput value={auditFile} onChange={(newFile) => setAuditFile(newFile)} />
+                  </div>
+                </div>
+              </div>
+              <div className={styles.grid}>
+                <button type="submit" className={styles.grid}>
+                  {" "}
+                  Contribute
+                </button>
+              </div>
+            </fieldset>
+          </form>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Formulario;
--- frontend/smart_audit/components/aiResults.js
+++ frontend/smart_audit/components/aiResults.js
@@ -1,18 +1,16 @@
-import React from 'react';
-const AIResults = ({text_content}) => {
+import React from "react";
+const AIResults = ({ text_content }) => {
+  if (text_content === "") {
+    return;
+  }
 
-    if (text_content === "") {
-        return 
-    }
+  return (
+    <section class="text_result_ai">
+      <h2>AI Generated Audit</h2>
 
-    return ( 
-        <section class="text_result_ai">
+      <p>{text_content}</p>
+    </section>
+  );
+};
 
-            <h2>AI Generated Audit</h2>
-
-            <p>{ text_content }</p>
-        </section>
-     );
-}
- 
 export default AIResults;
